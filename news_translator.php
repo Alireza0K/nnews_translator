cf6bb43eef77e94bc9002f1180cd886d566fd3ee
unknown <Alireza.karimi.new@gmail.com>
2021-10-25 19:51:39 +0330

    fix bug and add meta box

--- news_translator.php
+++ news_translator.php
@@ -18,8 +18,9 @@ define("INCLUDENEWSTEMPLATE",plugin_dir_path(__FILE__)."template/");
 // if not in public page dosent show and if in public page show . 
 if(is_admin()){
     include INCLUDENEWSPATH ."admin/show_menu_page.php";
-
+    include INCLUDENEWSPATH . "public/short_code_forms.php";
+    include INCLUDENEWSPATH . "admin/metabox.php";
 }else{
     include INCLUDENEWSPATH . "public/filter_word.php";
-    include INCLUDENEWSPATH . "public/short_code_forms.php";
+    
 }
