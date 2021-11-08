7823493b279f50606514b3deeddc0a185836ae01
unknown <Alireza.karimi.new@gmail.com>
2021-11-08 05:01:07 +0330

    add editor mode to site list menu

---
+++ template/admin/menu/edit_mode_site_list.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<style>
+    #first_form{
+        width: 310px;
+        background-color: #fff;
+        border: 1px gray solid;
+        margin: 0px auto;
+        margin-top: 20px;
+        border-radius: 10px;
+        padding: 10px 50px;
+    }
+</style>
+<body>
+    <div class="WRAP" id="first_form">
+        <form  method="post" style="text-align: center;margin-top:10px;">
+            <label for="new_website_name">
+                اسم جدید وبسایت :
+                <input type="text" name="new_website_name" id="new_website_name">
+            </label>
+            <br>
+            <br>
+            <label for="new_url">
+                آدرس جدید وبسایت :
+                <input type="text" name="new_url" id="new_url"> 
+            </label>
+            <div class="" style="margin:10px;"><button type="submit" name="save_edit_mode">ذخیره</button></div>
+        </form>
+    </div>
+</body>
+</html>
+<?php
+
+// if (isset($__POST["save_edit_mode"])) {
+//     $item = intval($_GET["item"]);
+//     $wpdb->update($wpdb->prefix."site_list",[
+//         "name_site"=>$_POST["new_website_name"],
+//         "url"=> $_POST["new_url"],
+//         $item
+//     ]);
+//     var_dump($item);
+// }
