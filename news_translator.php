ea24cd08548d720ebeeb039918167ce8d3c5feb7
unknown <Alireza.karimi.new@gmail.com>
2021-11-15 16:24:26 +0330

    add install pack to plugin

--- news_translator.php
+++ news_translator.php
@@ -15,6 +15,31 @@ define("INCLUDENEWSURL",plugin_dir_url(__FILE__)."assets/");
 define("INCLUDENEWSASSETS",plugin_dir_path(__FILE__)."assets/");
 define("INCLUDENEWSTEMPLATE",plugin_dir_path(__FILE__)."template/");
 
+
+function Create_table_site_list()
+{
+    global $wpdb;
+    $Customer_table_name = $wpdb->prefix . "site_list";
+    $collate = $wpdb->get_charset_collate();
+    $site_list_create_teble="
+    CREATE TABLE `{$Customer_table_name}` (
+        `id` int(21) NOT NULL,
+        `name_site` varchar(120) COLLATE utf8_persian_ci DEFAULT NULL,
+        `url` varchar(300) COLLATE utf8_persian_ci DEFAULT NULL
+      ) {$collate};
+    ";
+    require_once(ABSPATH."wp-admin/includes/upgrade.php");
+    dbDelta($site_list_create_teble);
+}
+
+function nnews_Active_plugin()
+{
+    Create_table_site_list();
+}
+
+register_activation_hook(__FILE__,"nnews_Active_plugin");
+
+
 // if not in public page dosent show and if in public page show . 
 if(is_admin()){
     include INCLUDENEWSPATH . "admin/show_menu_page.php";
